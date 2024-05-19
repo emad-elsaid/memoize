15766f9c689f41999991ba8a6644dcff134673a9
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-05-19 16:12:28 +0200

    adds an example

---
+++ factorial_example_test.go
@@ -0,0 +1,19 @@
+package memoize
+
+import (
+	"fmt"
+)
+
+func ExampleFactorial() {
+	var factorial func(uint64) uint64
+	factorial = New(func(i uint64) uint64 {
+		if i == 0 {
+			return 1
+		}
+
+		return i * factorial(i-1)
+	})
+
+	fmt.Println(factorial(10))
+	// Output: 3628800
+}
