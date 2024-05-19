5b5bc22b90e18bf6454fec6daa9e0fea50a96b3b
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2024-05-19 16:59:13 +0200

    add another example

--- factorial_example_test.go
+++ factorial_example_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// a function that calculates factorial recursively and memoize each input/output pair
 func ExampleNew_factorial() {
 	var factorial func(uint64) uint64
 	factorial = New(func(i uint64) uint64 {
@@ -16,4 +17,21 @@ func ExampleNew_factorial() {
 
 	fmt.Println(factorial(10))
 	// Output: 3628800
+}
+
+// Create a log function that prints the message only
+// once. no matter how many times it's called for the same message
+func ExampleNew_uniqueLogger() {
+	log := New(func(msg string) bool {
+		fmt.Println(msg)
+		return true
+	})
+
+	log("Hello World!")
+	log("Hello World!")
+	log("We came in peace")
+	log("We came in peace")
+
+	// Output: Hello World!
+	// We came in peace
 }
